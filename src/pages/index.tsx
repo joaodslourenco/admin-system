c92c25ee844c89625633d135ae2925cdc8c99d76
Joao Lourenco <joaodslourenco@gmail.com>
2022-01-06 09:39:12 -0300

    first commit, starting the project

---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="bg-gradient-to-r from-gray-500 to-green-600 flex h-screen items-center justify-center">
+      <h1>admin</h1>
+    </div>
+  )
+}
