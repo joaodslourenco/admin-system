d4b14c8f1d7c5cf90e5e6d6a5044a88f2562daca
Joao Lourenco <joaodslourenco@gmail.com>
2022-01-11 14:56:04 -0300

    user avatar added

---
+++ src/pages/perfil.tsx
@@ -0,0 +1,14 @@
+import Layout from '../components/template/Layout'
+
+export default function Perfil() {
+  return (
+    <div>
+      <Layout
+        titulo="Perfil"
+        subtitulo="Aqui você pode alterar os seus dados de usuário."
+      >
+        <h1>dados</h1>
+      </Layout>
+    </div>
+  )
+}
