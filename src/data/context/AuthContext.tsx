85a9ad2c9a16b93a14872d3b3519d67b1b9552ba
Joao Lourenco <joaodslourenco@gmail.com>
2022-01-11 15:58:25 -0300

    implemented cookies to keep user session

--- src/data/context/AuthContext.tsx
+++ src/data/context/AuthContext.tsx
@@ -1,7 +1,8 @@
 import { useRouter } from 'next/router'
-import { createContext, useState } from 'react'
+import { createContext, useEffect, useState } from 'react'
 import firebase from '../../firebase/config'
 import Usuario from '../../model/Usuario'
+import Cookies from 'js-cookie'
 
 interface AuthContextProps {
   usuario?: Usuario
@@ -24,21 +25,49 @@ async function usuarioNormalizado(
   }
 }
 
+function gerenciarCookie(logado: boolean) {
+  if (logado) {
+    Cookies.set('admin-template-auth', logado, {
+      expires: 7
+    })
+  } else {
+    Cookies.remove('admin-template-auth')
+  }
+}
+
 export function AuthProvider(props) {
-  const [usuario, setUsuario] = useState<Usuario>(null)
   const router = useRouter()
+  const [usuario, setUsuario] = useState<Usuario>(null)
+  const [carregando, setCarregando] = useState(true)
+
+  async function configurarSessao(usuarioFirebase) {
+    if (usuarioFirebase?.email) {
+      const usuario = await usuarioNormalizado(usuarioFirebase)
+      setUsuario(usuario)
+      gerenciarCookie(true)
+      setCarregando(false)
+      return usuario.email
+    } else {
+      setUsuario(null)
+      gerenciarCookie(false)
+      setCarregando(false)
+      return false
+    }
+  }
 
   async function loginGoogle() {
     const resp = await firebase
       .auth()
       .signInWithPopup(new firebase.auth.GoogleAuthProvider())
 
-    if (resp.user?.email) {
-      const usuario = await usuarioNormalizado(resp.user)
-      setUsuario(usuario)
-      router.push('/')
-    }
+    configurarSessao(resp.user)
+    router.push('/')
   }
+
+  useEffect(() => {
+    const cancelar = firebase.auth().onIdTokenChanged(configurarSessao)
+    return () => cancelar()
+  }, [])
 
   return (
     <AuthContext.Provider
